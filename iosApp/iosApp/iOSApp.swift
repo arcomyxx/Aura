2069baedc04639e5d3f4e50e437b2bf9b8519438
arcomyxx <thibault.dubois2@gmail.com>
2026-06-27 15:12:18 +0200

    feat(ios): init Firebase au lancement (FirebaseApp.configure + GoogleService-Info.plist bundle)

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,11 @@
 import SwiftUI
+import FirebaseCore
 
 @main
 struct iOSApp: App {
+    init() {
+        FirebaseApp.configure()   // initialise Firebase a partir de GoogleService-Info.plist
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
